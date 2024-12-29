8df99999d193fef0a972a57658877436a2305125
Spencer Boggs <spencerdboggs@gmail.com>
2024-12-29 01:24:57 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import os
 import time
 import json
 import webbrowser
+import keyboard
 import pyperclip
 import pyautogui
 import pygetwindow as gw
-import openlinks
 
 def create_saved_groups_folder():
     if not os.path.exists('saved-groups'):
@@ -62,7 +62,6 @@ def save_group_auto():
     else:
         print("No tabs detected or saved.")
 
-
 def open_group():
     files = [file for file in os.listdir('saved-groups') if file.endswith('.txt')]
     if not files:
@@ -91,6 +90,212 @@ def open_group():
         webbrowser.open(link)
         time.sleep(1)
 
+def open_group_sequentially():
+    files = [file for file in os.listdir('saved-groups') if file.endswith('.txt')]
+    if not files:
+        print('No groups saved yet.')
+        return
+
+    print('Select a group to open:')
+    for i, file in enumerate(files, start=1):
+        print(f'{i}. {file[:-4]}')
+
+    while True:
+        try:
+            choice = int(input('Enter the number of the group to open: '))
+            if 1 <= choice <= len(files):
+                break
+            else:
+                raise ValueError
+        except ValueError:
+            print('Invalid choice. Please enter a valid number.')
+
+    with open(f'saved-groups/{files[choice - 1]}', 'r') as f:
+        group = json.load(f)
+        file_name = files[choice - 1][:-4]
+
+    file = open(f'saved-groups/{file_name}-remaining.txt', "w")
+
+    webbrowser.open('https://www.google.com')
+
+    for link in group:
+        webbrowser.open(link)
+        keyboard.wait('space')
+        keyboard.press_and_release('ctrl+w')
+        print(f'Opening {link}')
+        remaining_file = open(f'saved-groups/{file_name}-remaining.txt', "r")
+        remaining_lines = remaining_file.readlines()
+        remaining_file.close()
+        remaining_file = open(f'saved-groups/{file_name}-remaining.txt', "w")
+        remaining_file.writelines(remaining_lines[1:])
+        remaining_file.close()
+        print(f'saved-groups/{file_name}-remaining.txt updated')
+
+    print('All links opened')
+    os.remove(f'saved-groups/{file_name}-remaining.txt')
+    print(f'saved-groups/{file_name}-remaining.txt deleted')
+
+def add_links_to_group():
+    files = [file for file in os.listdir('saved-groups') if file.endswith('.txt')]
+    if not files:
+        print('No groups saved yet.')
+        return
+
+    print('Select a group to add links to:')
+    for i, file in enumerate(files, start=1):
+        print(f'{i}. {file[:-4]}')
+
+    while True:
+        try:
+            choice = int(input('Enter the number of the group to open: '))
+            if 1 <= choice <= len(files):
+                break
+            else:
+                raise ValueError
+        except ValueError:
+            print('Invalid choice. Please enter a valid number.')
+            
+    with open(f'saved-groups/{files[choice - 1]}', 'r') as f:
+        group = json.load(f)
+
+    while True:
+        link = input('Enter a link to save in the group (enter nothing to stop): ').strip()
+        if link == '':
+            break
+        group.append(link)
+
+    with open(f'saved-groups/{files[choice - 1]}', 'w') as f:
+        json.dump(group, f)
+
+    print(f'Group "{files[choice - 1][:-4]}" saved successfully!')
+
+def add_links_to_group_auto():
+    files = [file for file in os.listdir('saved-groups') if file.endswith('.txt')]
+    if not files:
+        print('No groups saved yet.')
+        return
+
+    print('Select a group to add links to:')
+    for i, file in enumerate(files, start=1):
+        print(f'{i}. {file[:-4]}')
+
+    while True:
+        try:
+            choice = int(input('Enter the number of the group to open: '))
+            if 1 <= choice <= len(files):
+                break
+            else:
+                raise ValueError
+        except ValueError:
+            print('Invalid choice. Please enter a valid number.')
+            
+    with open(f'saved-groups/{files[choice - 1]}', 'r') as f:
+        group = json.load(f)
+
+    print("Ensure Google Chrome is active and all tabs are loaded.")
+    time.sleep(4)
+
+    max_tabs = 50
+    new_tabs = []
+
+    for _ in range(max_tabs):
+        pyautogui.hotkey('ctrl', 'l')
+        time.sleep(0.5)
+        pyautogui.hotkey('ctrl', 'insert')
+        time.sleep(0.5)
+
+        link = pyperclip.paste().strip()
+        if not link or link in new_tabs:
+            print("Duplicate or invalid link detected, stopping.")
+            break
+
+        new_tabs.append(link)
+        pyautogui.hotkey('ctrl', 'tab')
+        time.sleep(0.5)
+
+    for link in new_tabs:
+        group.append(link)
+
+    with open(f'saved-groups/{files[choice - 1]}', 'w') as f:
+        json.dump(group, f)
+
+    print(f'Group "{files[choice - 1][:-4]}" saved successfully! Total links saved: {len(group)}')
+
+def remove_tab_group_entries():
+    files = [file for file in os.listdir('saved-groups') if file.endswith('.txt')]
+    if not files:
+        print('No groups saved yet.')
+        return
+
+    print('Select a group to remove links from:')
+    for i, file in enumerate(files, start=1):
+        print(f'{i}. {file[:-4]}')
+
+    while True:
+        try:
+            choice = int(input('Enter the number of the group to open: '))
+            if 1 <= choice <= len(files):
+                break
+            else:
+                raise ValueError
+        except ValueError:
+            print('Invalid choice. Please enter a valid number.')
+            
+    with open(f'saved-groups/{files[choice - 1]}', 'r') as f:
+        group = json.load(f)
+
+    while True:
+        for i, link in enumerate(group):
+            print(f'{i}. {link}')
+
+        remove = input('Enter the number of the link to remove (enter nothing to stop): ').strip()
+        if remove == '':
+            break
+        try:
+            remove = int(remove)
+            if remove < 0 or remove >= len(group):
+                raise ValueError
+        except ValueError:
+            print('Invalid choice. Please enter a valid number.')
+            continue
+
+        group.pop(remove)
+
+    with open(f'saved-groups/{files[choice - 1]}', 'w') as f:
+        json.dump(group, f)
+
+    print(f'Group "{files[choice - 1][:-4]}" saved successfully!')
+
+def remove_duplicate_links():
+    files = [file for file in os.listdir('saved-groups') if file.endswith('.txt')]
+    if not files:
+        print('No groups saved yet.')
+        return
+
+    print('Select a group to remove links from:')
+    for i, file in enumerate(files, start=1):
+        print(f'{i}. {file[:-4]}')
+
+    while True:
+        try:
+            choice = int(input('Enter the number of the group to open: '))
+            if 1 <= choice <= len(files):
+                break
+            else:
+                raise ValueError
+        except ValueError:
+            print('Invalid choice. Please enter a valid number.')
+            
+    with open(f'saved-groups/{files[choice - 1]}', 'r') as f:
+        group = json.load(f)
+
+    group = list(set(group))
+
+    with open(f'saved-groups/{files[choice - 1]}', 'w') as f:
+        json.dump(group, f)
+
+    print(f'Group "{files[choice - 1][:-4]}" saved successfully!')
+
 def main():
     create_saved_groups_folder()
     while True:
@@ -99,7 +304,13 @@ def main():
         print('2. Save group automatically')
         print('3. Open group (All at once)')
         print('4. Open group (One at a time)')
-        print('5. Exit')
+        print('5. Add to a tab group manually')
+        print('    - Input links one at a time')
+        print('6. Add to a tab group automatically')
+        print('    - Will add all links you have open')
+        print('7. Remove tab group entries')
+        print('8. Remove duplicate links from group')
+        print('9. Exit')
         choice = input('Enter your choice: ').strip()
 
         if choice == '1':
@@ -109,8 +320,16 @@ def main():
         elif choice == '3':
             open_group()
         elif choice == '4':
-            openlinks.main()
+            open_group_sequentially()
         elif choice == '5':
+            add_links_to_group()
+        elif choice == '6':
+            add_links_to_group_auto()
+        elif choice == '7':
+            remove_tab_group_entries()
+        elif choice == '8':
+            remove_duplicate_links()
+        elif choice == '9':
             break
         else:
             print('Invalid choice.')
